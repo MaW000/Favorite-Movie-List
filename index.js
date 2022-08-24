1507c1739cb3d9e3a24f1343528c7e9dbfa787f5
matt wardlow <mwardlow0@gmail.com>
2022-08-23 20:42:13 -0400

    hosting

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const app = express()
 const path = require('path');
 const morgan = require('morgan')
+require('dotenv').config()
 app.use(morgan('dev'))
 app.use(express.json())
 
@@ -13,10 +14,16 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
 })
 
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true})
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.error(err));
-
-app.listen(process.env.PORT || 5000, () => {
-    console.log('This server is working')
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+  .then(() => {
+      console.log('DB Connection Succesfull');
+  })
+  .catch((err) => {
+      console.log(err.message)
+  })
+  app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server Started on Port ${process.env.PORT || 5000}`)
+  })
