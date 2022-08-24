253f7b07ee3c75ddaee36a3c40e66600983036c3
matt wardlow <mwardlow0@gmail.com>
2022-08-23 20:23:48 -0400

    hosting

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
 })
 
-mongoose.connect('mongodb+srv://matt:nEd5juQLYp2DEo2t@cluster0.zfgnmzv.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true})
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.error(err));
 
